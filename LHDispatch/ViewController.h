c6c255c9c24293dee9af076254fdd02b703578f4
李辉 <lihui@mucang.cn>
2017-05-08 14:19:57 +0800

    Initial Commit

---
+++ LHDispatch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LHDispatch
+//
+//  Created by 李辉 on 2017/5/8.
+//  Copyright © 2017年 木仓科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LHDispatch/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LHDispatch
+//
+//  Created by 李辉 on 2017/5/8.
+//  Copyright © 2017年 木仓科技. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
